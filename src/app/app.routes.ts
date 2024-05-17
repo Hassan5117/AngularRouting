fc9d69ce49f305eeb3c295878628779c9a2c3140
Hassan <hassansyed123@hotmail.com>
2024-05-17 15:33:38 -0400

    Added handling for not found pages, and programatically added a url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: FirstComponent},
-    {path: 'second', component: SecondComponent}
+    {path: 'second', component: SecondComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', component: NotFoundComponent}
+
 
 ]; // localhost:4200/first or /second
---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactComponent } from './contact.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ContactComponent,
+    
+  ],
+  exports: [
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    ContactComponent
+  ]
+})
+export class ContactModule { }
