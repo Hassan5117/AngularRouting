24700ea254ad4a1b841b8837293bbb69a51aea34
Hassan <hassansyed123@hotmail.com>
2024-05-17 15:46:31 -0400

    Provided data to routes using services

---
+++ src/app/products/productslist/productslist.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-productslist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './productslist.component.html',
+  styleUrl: './productslist.component.css'
+})
+export class ProductslistComponent implements OnInit {
+  products: any[] = [];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.store.getAllProducts().subscribe(products => {
+      this.products = products;
+    })
+    
+  }
+
+
+  constructor(private store: ProductsService) {}
+
+}
